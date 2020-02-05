e91ec76eae561de13159a2d9914d314af7d0c402
Brandon Vargas <brandon_vargas@homedepot.com>
2020-02-05 13:21:43 -0500

    making first call attempt

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
 var some_text = "I AM SOME TEXT, YES.";
 
+
+
 new Vue({
     el: '#app',
     data: {
-      m1: some_text
+      m1: some_text,
+      info: null
     },
     vuetify: new Vuetify(),
+    mounted () {
+        axios
+          .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+          .then(response => (this.info = response))
+      }
 });
