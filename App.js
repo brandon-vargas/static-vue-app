210c0c6276fa514511b94fd6de0c34f6e6996b3a
Brandon Vargas <brandon_vargas@homedepot.com>
2020-02-05 15:34:16 -0500

    added an ugly input box and button that connects html file with a js file that has vue and makes a get call

--- App.js
+++ App.js
@@ -6,9 +6,22 @@ new Vue({
     el: '#app',
     data: {
       m1: some_text,
-      info: null
+      info: null,
+      more_info: 'starting info',
+      name: 'idksomename',
     },
     vuetify: new Vuetify(),
+    methods: {
+      myClickHandler: function(e) {
+        // "this" here refers to the model
+        // alert("Hello " + this.name);
+        
+        var url = 'https://api.'+ this.name + '.com/v1/bpi/currentprice.json';
+        axios
+          .get(url)
+          .then(response => (this.more_info = response))
+        }    
+    },
     mounted () {
         axios
           .get('https://api.coindesk.com/v1/bpi/currentprice.json')
