b3c2914aa21496659d2ae5e37e11ccac8943ca5f
Brandon Vargas <brandon_vargas@homedepot.com>
2020-02-04 20:15:15 -0500

    first real commit

---
+++ components/navbar.js
@@ -0,0 +1,8 @@
+  
+import { NavbarTemplate } from '../templates/navbar-template.js'
+
+const Navbar = {
+  template: NavbarTemplate,
+}
+
+export { Navbar }
---
+++ templates/navbar-template.js
@@ -0,0 +1,8 @@
+const NavbarTemplate = `<nav>
+<ul>
+  <li><a class="active" href="#/">Home</a></li>
+  <li><a href="#/about">About</a></li>
+</ul>
+</nav>`
+
+export { NavbarTemplate }
